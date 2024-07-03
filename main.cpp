dbcb55feee2215091c59dea9aaec5fd3e3af5ad9
vikrant <v031661@gmail.com>
2024-07-04 01:25:01 +0530

    version-2

--- main.cpp
+++ main.cpp
@@ -1,80 +1,42 @@
 #include<iostream>
-#include<math.h>
-#include<vector>
+#include<cmath>
 using namespace std;
 
-//constructing a Node
-struct node{
-    node* prev;
-    int data;
-    node* next;
-};
-typedef struct node N;
+//int *order = new int;
 
-int noOfVertices(int order, int levels);
-node* CayleyTree(int *arr, int o, int h,int v);
+struct treeNode{
+    treeNode* parent = nullptr;
+    treeNode* childOne = nullptr;
+    treeNode* childTwo = nullptr;
+    int data;
+};
+int noOfVertices(int height, int order){
+    int result = 1;
+    for(int i = 0;i<height;i++){
+        result += (order+1)*pow((order),i);
+    }
+    return result;
+}
+treeNode *cayleyTree(int h, int o){
+    treeNode* root = new treeNode;
+//    root->data = ar[0];
+    
+    return root;
+}
+treeNode* insertDataCayleyTree(int *H, int * ar){
+    return nullptr;
+}
 int main(){
-    int height, order = 2;
+    int order = 2,height;
     cin>>height;
-    // for finding the no. of vertices
-    int V = noOfVertices(order, height);
-    //taking data for each vertex of tree
+    int V = noOfVertices(height, order);
     int arr[V];
-    for (int i = 0;i<V;i++){
-        // cin>>arr[i];
+    for(int i = 0;i<V;i++){
+//        cin>>arr[i];
         arr[i] = i;
     }
-    //forming a Cayley Tree
-    node* head = CayleyTree(arr, order, height, V);
+
+    treeNode* head = cayleyTree(height, order, arr);
+//    delete order;
     return 0;
-}
-
-int noOfVertices(int order, int levels){
-    int v = 1, d = order+1;
-
-    for (int i = 0; i<=levels-1;i++)
-    {
-        v += 3 * pow((d-1), i);
-    }
-
-    return v;
-}
-
-node* CayleyTree(int *arr, int o, int h, int v) {
-
-    int idx = 0;
-    vector<node*> queue;
-    node *root = new node[1];
-    root->data = arr[idx++];
-    root->next = new node[3];
-    for (int i = 0;i<3;i++){
-        root->next[i].data = arr[idx++];
-        root->next[i].prev = root;
-        queue.push_back(&root->next[i]);
-//        cout<<&(root->next[i]);
-    }
-    int *height = new int;
-    *height = h-1;
-    vector<node*> temp;
-    while( 1 )
-    {
-        if(*height == 0){break;}
-        for (int i = 0;i<queue.size();i++){
-            queue[i]->next = new node[o];
-            for(int j = 0;j<o;j++)
-            {
-                queue[i]->next[j].data = arr[idx++];
-                queue[i]->next[j].prev = (queue[i]);
-                temp.push_back(&queue[i]->next[j]);
-            }
-
-        }
-        queue.clear();
-        queue = temp;
-        *height -= 1;
-    }
-
-    delete height;
-
-    return root;
 }
